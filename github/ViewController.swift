3eeb924e107a0ff403031f3a48ab26378f465ed0
hamam002 <em.hamamoto@yahoo.com>
2015-06-30 20:05:50 -0500

    added a new println

--- github/ViewController.swift
+++ github/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         println("Does github work?")
+        println("Yes it does!")
     }
 
     override func didReceiveMemoryWarning() {
